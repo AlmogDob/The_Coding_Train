406122bfc47f4b4cd586ec13dcdcabfe559e07cf
Almog Dobrescu <dobalmog123@gmail.com>
2024-10-27 23:50:56 +0200

    starting coding challenge 179

---
+++ coding_challenge_179_-_elementary_cellular_Automata/main.c
@@ -0,0 +1,22 @@
+#define SETUP
+#define UPDATE
+#define RENDER
+#include "display.c"
+#define VEC2_IMPLEMENTATION
+#include "Vec2.h"
+
+void setup(void)
+{
+    game_is_running = 0;
+
+    printf("Hello, World!\n");
+}
+
+void update(void)
+{
+    
+}
+
+void render(void)
+{
+}
