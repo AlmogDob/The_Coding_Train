24417c44203c0e3caaac094d008e519560c59d42
Almog Dobrescu <dobalmog123@gmail.com>
2024-10-27 23:54:53 +0200

    adding matrix.h

--- coding_challenge_179_-_elementary_cellular_Automata/main.c
+++ coding_challenge_179_-_elementary_cellular_Automata/main.c
@@ -2,14 +2,16 @@
 #define UPDATE
 #define RENDER
 #include "display.c"
-#define VEC2_IMPLEMENTATION
-#include "Vec2.h"
+#define MATRIX_IMPLEMENTATION
+#include "Matrix.h"
 
 void setup(void)
 {
     game_is_running = 0;
 
     printf("Hello, World!\n");
+
+    
 }
 
 void update(void)
